b75548de60305c97bcd7822d2a01ded131bdf63c
lijingwei1209 <1004451027@qq.com>
2017-01-15 15:37:56 +0800

    hh

--- js4.js
+++ js4.js
@@ -1,13 +1,29 @@
 
 
 
-
-function test(a, b, c){
-    console.log(a+b+c);
-}
 function test(a, b){
     console.log(a+b);
 }
+function test(a, b, c){
+    console.log(a+b+c);
+}
 
+
+test(2,3,4);
 test(2,3,4,5);
 
+
+function testThis(){
+    return this;
+}
+
+console.log(testThis());
+
+
+var arr=[
+    {name: 'zhq'},
+    {age: 24},
+    {sex: '女'}
+]
+
+console.log(arr[0].name+arr[1].age+arr[2].sex);

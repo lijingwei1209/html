01c522fcfbf75f3ea57c019a330d2bb30ea7557d
ljw <1004451027@qq.com>
2017-01-20 10:55:36 +0800

    test

---
+++ js5.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ljw on 2017/1/9.
+ */
+var x = .3 - .2;
+var y = 0.2 - 0.1;
+console.log(x == y);
+console.log(x == 0.1);
+console.log(y == 0.1);
+
+var date = new Date(2017, 0, 9);
+console.log(date);
+
+var word = "test";
+word.len = 4;
+console.log(word.len);
+
+var comments = "    ";
+
+console.log(comments.trim().length);
